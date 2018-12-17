52876d637e98e1158e3569535f4e5e8b0c7f48ea
Lysxia <lysxia@gmail.com>
2018-12-16 23:09:05 -0500

    Fix scope of catch functions

--- src/IO_Utils.v
+++ src/IO_Utils.v
@@ -9,26 +9,26 @@ From SimpleIO Require Import
 Parameter catch_eof : forall {a : Type}, IO a -> IO (option a).
 Extract Constant catch_eof =>
   "(fun io k ->
-     try io (fun a -> k (Some a)) with
-     | End_of_file -> k None)".
+     k (try Obj.magic io (fun a -> Some a) with
+        | End_of_file -> None))".
 
 Parameter catch_invalid_arg : forall {a : Type}, IO a -> IO (option a).
 Extract Constant catch_invalid_arg =>
   "(fun io k ->
-     try io (fun a -> k (Some a)) with
-     | Invalid_argument _ -> k None)".
+     k (try Obj.magic io (fun a -> Some a) with
+        | Invalid_argument _ -> None))".
 
 Parameter catch_failure : forall {a : Type}, IO a -> IO (option a).
 Extract Constant catch_failure =>
   "(fun io k ->
-     try io (fun a -> k (Some a)) with
-     | Failure _ -> k None)".
+     k (try Obj.magic io (fun a -> Some a) with
+        | Failure _ -> None))".
 
 Parameter catch_not_found : forall {a : Type}, IO a -> IO (option a).
 Extract Constant catch_not_found =>
   "(fun io k ->
-     try io (fun a -> k (Some a)) with
-     | Not_found -> k None)".
+     k (try Obj.magic io (fun a -> Some a) with
+        | Not_found -> None)".
 
 (** * Manipulating strings *)
 
