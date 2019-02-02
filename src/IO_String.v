89bc198afa367e5a4b730fab6f598ec528c5dbab
Lysxia <lysxia@gmail.com>
2019-02-02 12:05:25 -0500

    Add documentation in IO_String

--- src/IO_String.v
+++ src/IO_String.v
@@ -1,3 +1,12 @@
+(** The [String] module from OCaml's standard library.
+
+    Operations are meant to be used qualified, e.g., [OString.length].
+
+    The inner module is named [OString] to avoid the conflict with
+    with [Coq.Strings.String].
+ *)
+
+(* begin hide *)
 From Coq Require Import
      Strings.String
      Strings.Ascii
@@ -7,39 +16,52 @@ From SimpleIO Require Import
      IO_Pervasives.
 
 Extraction Blacklist Bytes Pervasives String .
+(* end hide *)
 
 Module OString.
 
+(** Length of an [ocaml_string]. *)
 Parameter length : ocaml_string -> int.
 Extract Inlined Constant length => "String.length".
 
-(* Equals [None] if out of bounds. *)
+(** Index into an [ocaml_string].
+
+    Equals [None] if out of bounds. *)
 Parameter get_opt : ocaml_string -> int -> option char.
 Extract Constant get_opt =>
   "fun s i -> try Some (String.get s i)
               with Invalid_argument _ -> None".
 
-(* Concatenates strings with a separator. *)
+(** Concatenates strings with a separator. *)
 Parameter concat : ocaml_string -> list ocaml_string -> ocaml_string.
 Extract Inlined Constant concat => "String.concat".
 
+(** ** Unsafe functions *)
+
 Module Unsafe.
 
-(* Throws an exception if out of bounds. *)
+(** Index into an [ocaml_string], without an [option] wrapper.
+
+    Throws an exception if out of bounds. *)
 Parameter get : ocaml_string -> int -> char.
 Extract Inlined Constant get => "String.get".
 
-(* Throws an exception if [n < 0] or [n > Sys.max_string_length]. *)
+(** Create a string of repeated characters.
+
+    Throws an exception if [n < 0] or [n > Sys.max_string_length]. *)
 Parameter make : int -> char -> ocaml_string.
 Extract Inlined Constant make => "String.make".
 
-(* Throws an exception if [n < 0] or [n > Sys.max_string_length]. *)
+(** Create a string with a function from indices to characters.
+
+    Throws an exception if [n < 0] or [n > Sys.max_string_length]. *)
 Parameter init : int -> (int -> char) -> ocaml_string.
 Extract Inlined Constant init => "String.init".
 
-(* [sub i len s] is the substring of [s] with length [len]
-   starting at index [i].
-   Throws an exception if out of bounds. *)
+(** [sub i len s] is the substring of [s] with length [len]
+    starting at index [i].
+
+    Throws an exception if out of bounds. *)
 Parameter sub : ocaml_string -> int -> int -> ocaml_string.
 Extract Inlined Constant sub => "String.sub".
 
