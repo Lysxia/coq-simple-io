999ae06200a37bbe0a694bc064fe0c1e1c1abe5e
Lysxia <lysxia@gmail.com>
2019-05-30 09:10:04 -0400

    Make stricter tests

--- test/TestPervasives.v
+++ test/TestPervasives.v
@@ -7,10 +7,30 @@ From Coq.extraction Require Import
 From SimpleIO Require Import SimpleIO IO_UnsafeNat.
 Import IO.Notations.
 
-Open Scope string_scope.
+Local Open Scope string_scope.
 
 Parameter int_constant : int.
 Extract Constant int_constant => "3".
+
+Class Eq (a : Type) :=
+  eqb : a -> a -> bool.
+
+Class Print (a : Type) :=
+  print : a -> IO unit.
+
+Instance Eq_nat : Eq nat := Nat.eqb.
+Instance Print_nat : Print nat := print_nat.
+
+Instance Eq_string : Eq string := String.eqb.
+Instance Print_string : Print string := print_string.
+
+Definition assert_eq {a} `{Eq a} `{Print a} (expect actual : a) : IO unit :=
+  if eqb expect actual then
+    IO.ret tt
+  else
+    (print_string "Expected: ";; print expect;; print_newline;;
+     print_string "Actual  : ";; print actual;; print_newline;;
+     exit_nat 1).
 
 Definition main : IO unit :=
   print_char (char_of_ascii "a");; print_newline;;
@@ -22,16 +42,16 @@ Definition main : IO unit :=
   close_out h;;
   h <- open_in "/tmp/test_file.txt";;
   n <- input_byte_nat h;;
-  print_nat n;; print_newline;;
   close_in h;;
+  assert_eq 65 n;;
   r <- new_ref 13;;
   incr_ref_nat r;;
   i <- read_ref r;;
-  print_nat i;; print_newline;;
+  assert_eq 14 i;;
   write_ref r 1;;
   decr_ref_nat r;;
   j <- read_ref r;;
-  print_nat j;; print_newline;;
+  assert_eq 0 j;;
   exit_nat 0.
 
 Definition run_main : io_unit := IO.unsafe_run main.
