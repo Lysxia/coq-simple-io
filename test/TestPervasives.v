1970eb1e77f94411d6d513c54719fc6d298a454f
Lysxia <lysxia@gmail.com>
2019-05-30 09:44:30 -0400

    Add unit tests for OString

--- test/TestPervasives.v
+++ test/TestPervasives.v
@@ -23,12 +23,30 @@ Class Print (a : Type) :=
 Instance Eq_nat : Eq nat := Nat.eqb.
 Instance Print_nat : Print nat := print_nat.
 
+Instance Eq_option {a : Type} `{Eq a} : Eq (option a) :=
+  fun x y =>
+    match x, y with
+    | None, None => true
+    | Some x, Some y => eqb x y
+    | _, _ => false
+    end.
+Instance Print_option {a : Type} `{Print a} : Print (option a) :=
+  fun x =>
+    match x with
+    | None => print_string "None"
+    | Some x => print_string "Some (";; print x;; print_string ")"
+    end.
+
 Instance Eq_string : Eq string := String.eqb.
 Instance Print_string : Print string := print_string.
 
+Instance Eq_ostring : Eq ocaml_string := ostring_eqb.
+Instance Print_ostring : Print ocaml_string :=
+  fun s => print_string (OString.escaped s).
+
 Instance Eq_char : Eq char := char_eqb.
 Instance Print_char : Print char :=
-  fun c => print_string (OString.escaped (OString.of_list [c])).
+  fun c => print (OString.of_list [c]).
 
 Definition assert_eq {a} `{Eq a} `{Print a} (expect actual : a) : IO unit :=
   if eqb expect actual then
@@ -37,6 +55,8 @@ Definition assert_eq {a} `{Eq a} `{Print a} (expect actual : a) : IO unit :=
     (print_string "Expected: ";; print expect;; print_newline;;
      print_string "Actual  : ";; print actual;; print_newline;;
      exit_nat 1).
+
+Coercion int_of_nat : nat >-> int.
 
 Definition main : IO unit :=
   (* Pervasives *)
@@ -61,6 +81,18 @@ Definition main : IO unit :=
   j <- read_ref r;;
   assert_eq 0 j;;
 
+  (* String *)
+
+  let s : ocaml_string := "test" in
+  assert_eq 4 (nat_of_int (OString.length s));;
+  assert_eq (Some ("t"%char : char)) (OString.get_opt s 3);;
+  assert_eq None (OString.get_opt s 4);;
+  assert_eq ("test,test" : ocaml_string) (OString.concat "," [s;s]);;
+
+  let nl := String "010" "" in
+  let s' := OString.escaped (nl ++ nl)%string in
+  assert_eq ("\n\n" : ocaml_string) s';;
+
   (* Bytes *)
 
   b <- OBytes.of_string "test";;
@@ -70,9 +102,8 @@ Definition main : IO unit :=
   let n := nat_of_int (OBytes.length b) in
   assert_eq 4 n;;
 
-  let i0 := int_of_nat 0 in
-  OBytes.set b i0 "r"%char;;
-  r <- OBytes.get b i0;;
+  OBytes.set b 0 "r"%char;;
+  r <- OBytes.get b 0;;
   assert_eq ("r"%char : char) r;;
 
   b <- OBytes.create (int_of_nat 1);;
