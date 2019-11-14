19592e110ceae81d31d40a61a4e7df6b3fe64ae8
Lysxia <lysxia@gmail.com>
2019-11-14 10:35:58 -0500

    test: Improve coverage of extraction test

--- test/TestExtraction.v
+++ test/TestExtraction.v
@@ -7,16 +7,17 @@ From SimpleIO Require Import
   IO_Unsafe
   IO_UnsafeNat.
 
+(* Make sure these are not inlined constants so the whole module gets extracted. *)
 Definition test :=
   ( IO_Stdlib.print_string,
     @IO_Exceptions.catch_eof,
-    IO_RawChar.int_of_ascii,
-    IO_String.OString.length,
+    IO_RawChar.to_ostring,
+    IO_String.OString.get_opt,
     IO_Bytes.OBytes.get,
     IO_Random.ORandom.int,
-    IO_Unix.OUnix.error,
+    IO_Unix.OUnix.sleep,
     IO_Float.OFloat.of_int,
-    IO_Unsafe.unsafe_int_div,
+    IO_Unsafe.unsafe_int_div, (* This module contains only inlined constants... *)
     IO_UnsafeNat.print_nat
   ).
 
