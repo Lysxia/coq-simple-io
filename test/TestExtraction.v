bb61798e695366912c595ef4bed4e6e759e09407
Yishuai Li <yishuai@upenn.edu>
2020-03-02 22:18:48 -0500

    Add Sys module

--- test/TestExtraction.v
+++ test/TestExtraction.v
@@ -4,6 +4,7 @@ From SimpleIO Require Import
   IO_Random
   IO_Unix
   IO_Float
+  IO_Sys
   IO_Unsafe
   IO_UnsafeNat.
 
@@ -17,6 +18,7 @@ Definition test :=
     IO_Random.ORandom.int,
     IO_Unix.OUnix.sleep,
     IO_Float.OFloat.of_int,
+    IO_Sys.OSys.getenv,
     IO_Unsafe.unsafe_int_div, (* This module contains only inlined constants... *)
     IO_UnsafeNat.print_nat
   ).
@@ -27,5 +29,6 @@ Separate Extraction test
   IO_Random
   IO_Unix
   IO_Float
+  IO_Sys
   IO_Unsafe
   IO_UnsafeNat.
