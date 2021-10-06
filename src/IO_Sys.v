8bb662dfb1f312d3d3f1614e89804b4f8c830f26
Yishuai Li <yishuai@upenn.edu>
2021-10-06 02:25:36 -0400

    Sys: add command

--- src/IO_Sys.v
+++ src/IO_Sys.v
@@ -14,6 +14,24 @@ From SimpleIO Require Import
 
 Module OSys.
 
+(** Execute the given shell command and return its exit code.
+  The argument of [Sys.command] is generally the name of a
+  command followed by zero, one or several arguments, separated
+  by whitespace.  The given argument is interpreted by a
+  shell: either the Windows shell [cmd.exe] for the Win32 ports of
+  OCaml, or the POSIX shell [sh] for other ports.  It can contain
+  shell builtin commands such as [echo], and also special characters
+  such as file redirections [>] and [<], which will be honored by the
+  shell.
+  Conversely, whitespace or special shell characters occurring in
+  command names or in their arguments must be quoted or escaped
+  so that the shell does not interpret them.  The quoting rules vary
+  between the POSIX shell and the Windows shell.
+  The [Filename.quote_command] performs the appropriate quoting
+  given a command name, a list of arguments, and optional file redirections.
+*)
+Parameter command: ocaml_string -> IO int.
+
 (** Return the value associated to a variable in the process environment.
     Raise [Not_found] if the variable is unbound. *)
 Parameter getenv : ocaml_string -> IO ocaml_string.
@@ -28,6 +46,7 @@ Parameter time : IO float.
 
 (** ** Extraction *)
 
+Extract Constant command    => "fun c k -> k (Sys.command    c)".
 Extract Constant getenv     => "fun e k -> k (Sys.getenv     e)".
 Extract Constant getenv_opt => "fun e k -> k (Sys.getenv_opt e)".
 Extract Constant time       => "fun   k -> k (Sys.time      ())".
